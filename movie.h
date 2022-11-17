15933ea2f81d7ede12309ba1672b2d875cfe7b71
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-16 23:22:06 -0500

    movie and timeslot headers

---
+++ movie.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+enum Genre {
+  ACTION, COMEDY, DRAMA, ROMANCE, THRILLER
+};
+
+class Movie {
+public:
+  std::string title;
+  Genre genre;
+  int duration;
+};
---
+++ timeslot.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "movie.h"
+#include "time.h"
+
+class TimeSlot {
+public:
+  Movie movie;     // what movie
+  Time startTime;  // when it starts
+};
