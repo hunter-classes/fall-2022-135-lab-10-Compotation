80fa150f00dcb4094029bd632902044f0cf03d1a
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-16 21:39:43 -0500

    overload ==

--- time.h
+++ time.h
@@ -7,4 +7,7 @@ public:
   friend std::ostream& operator <<(std::ostream &os, Time const &t) {
     return os << t.h << ":" << t.m;
   };
+  friend bool operator ==(const Time& lhs, const Time& rhs) {
+    return lhs.h == rhs.h && lhs.m == rhs.m;
+  }
 };
