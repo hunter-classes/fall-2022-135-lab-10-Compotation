6d102856b23b7470dcef0ff426cd1b8e2894c93c
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-16 22:12:46 -0500

    task b

--- main.cpp
+++ main.cpp
@@ -7,5 +7,8 @@ int main() {
   Time t1340 = {13, 40};
   Time t1030 = {10, 30};
   std::cout << t1030 << " to " << t1340 << " is " << minutesUntil(t1030, t1340) << " minutes" << "\n";
+
+  Time t0810 = {8, 10};
+  std::cout << t0810 << " + 75 minutes is: " << addMinutes(t0810, 75) << "\n";
   return 0;
 }
