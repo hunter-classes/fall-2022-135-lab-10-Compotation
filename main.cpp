1c5c30146638fbc849747485c857e6333a8d5b1a
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-16 22:18:53 -0500

    add additional output text for minutesSinceMidnight

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
   Time t0230 = {2, 30};
-  std::cout << minutesSinceMidnight(t0230) << "\n";
+  std::cout << t0230 << " is " << minutesSinceMidnight(t0230) << " minutes after midnight" << "\n";
   Time t1340 = {13, 40};
   Time t1030 = {10, 30};
   std::cout << t1030 << " to " << t1340 << " is " << minutesUntil(t1030, t1340) << " minutes" << "\n";
