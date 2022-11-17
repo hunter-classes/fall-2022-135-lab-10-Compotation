e527d16028fbe03c2776843b9ecf67b156e3ec51
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-16 20:20:34 -0500

    overload cout

--- time.h
+++ time.h
@@ -1,5 +1,10 @@
+#include "ostream"
+
 class Time {
 public:
   int h;
   int m;
+  friend std::ostream& operator <<(std::ostream &os, Time const &t) {
+    return os << t.h << ":" << t.m;
+  };
 };
